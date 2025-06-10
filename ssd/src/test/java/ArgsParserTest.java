d7c9fb0dc7cd15a266d47e5e38178a60df9b906b
BROWNLEE99 <brownpug126@gmail.com>
2025-06-11 08:40:52 +0900

    [test]: 읽기요청의 인자는 2개 입력값2개가 아닐때 에러 - RED

--- ssd/src/test/java/ArgsParserTest.java
+++ ssd/src/test/java/ArgsParserTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,23 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArgsParserTest {
     // TODO RuntimeException -> output.txt.에 Error를 저장하는 것으로 변경
 
+    private static final String INVALID_FIRST_ARG[] = {"Q", "12"};
+    private static final String VALID_READ_ARGS[] = {"R", "56"};
+
     @Test
     void 인자의_맨앞이_R_혹은_W가_아니면_에러() {
-        String args[] = {"Q", "12"};
-
-        assertThrows(RuntimeException.class, () -> ArgsParser.main(args));
+        assertThrows(RuntimeException.class, () -> ArgsParser.main(INVALID_FIRST_ARG));
     }
 
     @Test
-    @Disabled
     void 읽기요청의_인자는2개_입력값2개일때() {
-
+        assertDoesNotThrow(() -> ArgsParser.main(VALID_READ_ARGS));
     }
 
     @Test
-    @Disabled
     void 읽기요청의_인자는2개_입력값2개가아닐때_에러() {
-
+        String args[] = {"R", "12", "1"};
+        assertThrows(RuntimeException.class, () -> {
+            ArgsParser.main(args);
+        });
     }
 
     @Test
