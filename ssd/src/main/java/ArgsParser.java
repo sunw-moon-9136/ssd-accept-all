1a9116e32d95849cf03c6a9373de98ed8decad92
BROWNLEE99 <brownpug126@gmail.com>
2025-06-10 17:00:16 +0900

    [test]: 인자의 맨 앞은 R 혹은 W - GREEN

--- ssd/src/main/java/ArgsParser.java
+++ ssd/src/main/java/ArgsParser.java
@@ -1,5 +1,11 @@
+import java.util.Objects;
+
 public class ArgsParser {
     public static void main(String[] args) {
-        
+        if (args[0].equals("R") || args[0].equals("W")) {
+;
+        } else {
+            throw new RuntimeException();
+        }
     }
 }
