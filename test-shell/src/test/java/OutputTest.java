0132f215c4c432d1c789c791ec9ba8efd4fa4218
yuri <yuri@shine.com>
2025-06-11 08:40:05 +0900

    [test]: shell output 파일 확인하기#2-RED

--- test-shell/src/test/java/OutputTest.java
+++ test-shell/src/test/java/OutputTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+
+@ExtendWith(MockitoExtension.class)
 class OutputTest {
 
     @Mock
@@ -15,6 +20,14 @@ class OutputTest {
         Output o = new Output();
         boolean expected = true;
         boolean act = o.existFileCheck();
+        assertEquals(expected, act);
+    }
+
+    @Test
+    void output_파일이_없으면_FAIL() throws IOException {
+        when(f.existFileCheck()).thenReturn(false);
+        boolean expected = false;
+        boolean act = f.existFileCheck();
         assertEquals(expected, act);
     }
 
