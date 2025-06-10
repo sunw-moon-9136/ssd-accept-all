8f726a0b5fa221ed8fc34be73fa3e1671a97f8f5
yuri <yuri@shine.com>
2025-06-11 07:31:44 +0900

    [refactor]: shell output 파일 확인하기#1

--- test-shell/src/main/java/Output.java
+++ test-shell/src/main/java/Output.java
@@ -1,9 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class Output {
 
+    final static String OUTPUT_FILE_PATH = "C:\\Users\\User\\Documents\\output.txt";
 
-    public boolean existFileCheck() {
+    public boolean existFileCheck() throws IOException {
 
-        return true;
+        Path path = Paths.get(OUTPUT_FILE_PATH);
+
+        if (Files.exists(path)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
--- test-shell/src/test/java/OutputTest.java
+++ test-shell/src/test/java/OutputTest.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OutputTest {
 
+    @Mock
+    Output f;
+
     @Test
-    void output_파일이_있으면_PASS() {
+    void output_파일이_있으면_PASS() throws IOException {
         Output o = new Output();
         boolean expected = true;
         boolean act = o.existFileCheck();
