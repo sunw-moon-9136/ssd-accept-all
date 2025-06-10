8e070cd976a708844db035a7a4197e7e99b881cd
sunw.moon <sunw.moon@samsung.com>
2025-06-11 08:41:20 +0900

    [feat]: 예상되는 Argument Exception Case 구현 - GREEN

--- ssd/src/main/java/FileDriver.java
+++ ssd/src/main/java/FileDriver.java
@@ -1,11 +1,25 @@
 public class FileDriver implements Driver {
+
+    public static final String OUTPUT_FILE_NAME = "output.txt";
+    public static final String NAND_FILE_NAME = "nand.txt";
+
     @Override
     public String read(String file) {
+        if (file == null || file.isEmpty())
+            throw new IllegalArgumentException();
+
+        if (!file.equals(OUTPUT_FILE_NAME) && !file.equals(NAND_FILE_NAME))
+            throw new IllegalArgumentException();
+
         return "";
     }
 
     @Override
     public void write(String file, byte[] bytes) {
+        if (file == null || file.isEmpty())
+            throw new IllegalArgumentException();
 
+        if (!file.equals(OUTPUT_FILE_NAME) && !file.equals(NAND_FILE_NAME))
+            throw new IllegalArgumentException();
     }
 }
