d6134e693e978b00a286e4677b8032c934655f98
sunw.moon <sunw.moon@samsung.com>
2025-06-11 17:17:59 +0900

    [test]: Test Scenario 1 기능 구현(2) - RED

--- test-shell/src/test/java/FullWriteAndReadCompareTest.java
+++ test-shell/src/test/java/FullWriteAndReadCompareTest.java
@@ -4,11 +4,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FullWriteAndReadCompareTest {
@@ -16,10 +16,18 @@ class FullWriteAndReadCompareTest {
     @Mock
     RunCommand runCommand;
 
+    @Mock
+    Output output;
+
     @Test
-    void 정상적으로_모든_readCompare가_성공한_경우_return_true() throws IOException, InterruptedException {
-        ITestScenario testScenario = new FullWriteAndReadCompare(runCommand);
-        doNothing().when(runCommand).execute(any());
+    void 정상적으로_모든_readCompare가_성공한_경우_return_true()  {
+        
+        ITestScenario testScenario = new FullWriteAndReadCompare(runCommand, output);
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        doReturn(true).when(runCommand).execute(any());
+        doAnswer(invocationOnMock -> {
+            return String.format("LBA %02d : %s", atomicInteger.get(), makeHex(atomicInteger.getAndIncrement() / 5));
+        }).when(output).checkResult(anyString());
 
         boolean actual = testScenario.run();
 
@@ -27,9 +35,9 @@ class FullWriteAndReadCompareTest {
     }
 
     @Test
-    void 하나의_readCompare라도_실패한_경우_return_false() throws IOException, InterruptedException {
-        ITestScenario testScenario = new FullWriteAndReadCompare(runCommand);
-        doNothing().when(runCommand).execute(any());
+    void 하나의_readCompare라도_실패한_경우_return_false()  {
+        ITestScenario testScenario = new FullWriteAndReadCompare(runCommand, output);
+        doReturn(true).when(runCommand).execute(any());
 
         boolean actual = testScenario.run();
 
@@ -37,12 +45,16 @@ class FullWriteAndReadCompareTest {
     }
 
     @Test
-    void runCommand에서_Exception이_발생한_경우_return_false() throws IOException, InterruptedException {
-        ITestScenario testScenario = new FullWriteAndReadCompare(runCommand);
+    void runCommand에서_Exception이_발생한_경우_return_false()  {
+        ITestScenario testScenario = new FullWriteAndReadCompare(runCommand, output);
         doThrow(new IOException()).when(runCommand).execute(any());
 
         boolean actual = testScenario.run();
 
         assertThat(actual).isFalse();
+    }
+
+    private String makeHex(int num) {
+        return String.format("0x%02d%02d%02d%02d", num, num, num, num);
     }
 }
