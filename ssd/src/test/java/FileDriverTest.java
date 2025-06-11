c8b9e1088d289058656213fc6a626b176b07cb8a
sunw.moon <sunw.moon@samsung.com>
2025-06-11 09:06:35 +0900

    [test]: Write 기능 구현 - RED

--- ssd/src/test/java/FileDriverTest.java
+++ ssd/src/test/java/FileDriverTest.java
@@ -2,11 +2,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FileDriverTest {
 
-    public static final byte[] TEST_BYTES = {1, 2, 3, 4};
+    public static final byte[] TEST_BYTES = "Hello, World!".getBytes(StandardCharsets.UTF_8);
 
     private FileDriver fileDriver;
 
@@ -54,6 +62,32 @@ class FileDriverTest {
         void 잘못된_종류의_파일_write를_수행한_경우() {
             assertThatThrownBy(() -> fileDriver.write(INVALID_FILE_NAME, TEST_BYTES))
                     .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    class WriteTest {
+
+        @Test
+        void 대상_파일이_존재하지_않는_경우_파일_새로_생성하여_write() throws IOException {
+            Path path = Paths.get(FileDriver.NAND_FILE_NAME);
+            Files.deleteIfExists(path);
+
+            fileDriver.write(FileDriver.NAND_FILE_NAME, TEST_BYTES);
+
+            assertThat(Files.exists(path)).isTrue();
+            assertThat(Files.readString(path)).isEqualTo(new String(TEST_BYTES, StandardCharsets.UTF_8));
+        }
+
+        @Test
+        void 대상_파일이_존재하는_경우_파일_덮어쓰기하여_write() throws IOException {
+            Path path = Paths.get(FileDriver.NAND_FILE_NAME);
+            Files.write(path, "Bye, World!".getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
+
+            fileDriver.write(FileDriver.NAND_FILE_NAME, TEST_BYTES);
+
+            assertThat(Files.exists(path)).isTrue();
+            assertThat(Files.readString(path)).isEqualTo(new String(TEST_BYTES, StandardCharsets.UTF_8));
         }
     }
 }
