dac85aaadefa24747b58118c4109db003f6549c3
BROWNLEE99 <brownpug126@gmail.com>
2025-06-11 09:45:27 +0900

    [test]: 읽기요청의 인자는 2개 입력값2개가 아닐때 에러 - REFACTOR / Exception-> driver.write()

--- ssd/src/main/java/ArgsParser.java
+++ ssd/src/main/java/ArgsParser.java
@@ -1,4 +1,29 @@
 public class ArgsParser {
+    private Driver driver;
+    private ReadWritable disk;
+
+    public ArgsParser() {
+        this.driver = new FileDriver();
+        this.disk = new Ssd();
+    }
+
+    public ArgsParser(Driver driver, ReadWritable disk) {
+        this.driver = driver;
+        this.disk = disk;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public void setDisk(Ssd disk) {
+        this.disk = disk;
+    }
+
+    public void error() {
+        driver.write("ssd_output.txt", "ERROR".getBytes());
+    }
+
     public static boolean isValidModeCommand(String arg) {
         return (arg.equals("R") || arg.equals("W"));
     }
@@ -7,12 +32,16 @@ public class ArgsParser {
         return isValidModeCommand(args[0]);
     }
 
-    public static void main(String[] args) {
-        if(!isValidArgs(args)) throw new RuntimeException();
-        if(args[0] == "R") {
-            if(args.length != 2) throw new RuntimeException();
-        } else if(args[0] == "W") {
+    public void run(String[] args) {
+        try {
+            if(!isValidArgs(args)) throw new IllegalArgumentException();
+            if(args[0].equals("R")) {
+                if(args.length != 2) throw new IllegalArgumentException();
+            } else if(args[0].equals("W")) {
 
+            }
+        } catch(Exception e) {
+            error();
         }
     }
 }
--- ssd/src/test/java/ArgsParserTest.java
+++ ssd/src/test/java/ArgsParserTest.java
@@ -1,30 +1,65 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ArgsParserTest {
     // TODO RuntimeException -> output.txt.에 Error를 저장하는 것으로 변경
 
+    ArgsParser parser;
+
+    @Mock
+    Driver mockDriver;
+
+    @BeforeEach
+    void setUp() {
+        parser = new ArgsParser();
+        parser.setDriver(mockDriver);
+    }
+
     private static final String INVALID_FIRST_ARG[] = {"Q", "12"};
     private static final String VALID_READ_ARGS[] = {"R", "56"};
+    private static final String INVALID_READ_ARGS_CNT[] = {"R", "12", "77", "(!"};
+
+    @Test
+    void ArgsParser에서_에러가나면_Driver_Write를_호출() {
+        doNothing().when(mockDriver).write(anyString(), any());
+
+        parser.error();
+
+        verify(mockDriver, times(1)).write(anyString(), any());
+    }
 
     @Test
     void 인자의_맨앞이_R_혹은_W가_아니면_에러() {
-        assertThrows(RuntimeException.class, () -> ArgsParser.main(INVALID_FIRST_ARG));
+        doNothing().when(mockDriver).write(anyString(), any());
+
+        parser.run(INVALID_FIRST_ARG);
+
+        verify(mockDriver, times(1)).write(anyString(), any());
     }
 
     @Test
     void 읽기요청의_인자는2개_입력값2개일때() {
-        assertDoesNotThrow(() -> ArgsParser.main(VALID_READ_ARGS));
+        parser.run(VALID_READ_ARGS);
+
+        verify(mockDriver, never()).write(anyString(), any());
     }
 
     @Test
     void 읽기요청의_인자는2개_입력값2개가아닐때_에러() {
-        String args[] = {"R", "12", "1"};
-        assertThrows(RuntimeException.class, () -> {
-            ArgsParser.main(args);
-        });
+        doNothing().when(mockDriver).write(anyString(), any());
+
+        parser.run(INVALID_READ_ARGS_CNT);
+
+        verify(mockDriver, times(1)).write(anyString(), any());
     }
 
     @Test
