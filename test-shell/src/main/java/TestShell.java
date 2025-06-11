2f16fead0741be4582f3f38102b2d5df209cce44
kimdy003 <kyl123451@naver.com>
2025-06-11 13:42:31 +0900

    [refactor] 유효성 검사 매소드 구조 변경

--- test-shell/src/main/java/TestShell.java
+++ test-shell/src/main/java/TestShell.java
@@ -1,61 +1,75 @@
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class TestShell {
     public static final String INVALID_COMMAND = "INVALID COMMAND";
+    public static final String[] REGISTERED_COMMAND = new String[]{"read", "write", "fullread", "fullwrite", "help", "exit"};
+    public static final String[] ONE_LENGTH_COMMAND = new String[]{"fullread", "help", "exit"};
+    public static final String[] COMMAND_LBA = new String[]{"read"};
+    public static final String[] COMMAND_DATA = new String[]{"fullwrite"};
+    public static final String[] COMMAND_LBA_DATA = new String[]{"write"};
 
     public String runTestShell(Scanner input) {
         System.out.print(">> ");
         String command = input.nextLine().trim();
 
-        if (!validCommand(command)) return INVALID_COMMAND;
+        if (isNullEmpty(command)) return INVALID_COMMAND;
+        if (isValidCommand(command)) return INVALID_COMMAND;
         return command;
     }
 
-    private boolean validCommand(String command) {
-        if (command == null || command.isEmpty()) return false;
+    private boolean isNullEmpty(String command) {
+        return command == null || command.isEmpty();
+    }
 
+    private boolean isValidCommand(String command) {
         String[] parts = command.split("\\s+");
-        if (!checkRegisteredCommand(parts[0])) return false;
-        if (!parts[0].matches("[a-z]+")) return false;
 
-        if (Arrays.asList("fullread", "help", "exit").contains(parts[0]) && parts.length != 1) return false;
-        if (Objects.equals(parts[0], "read") && (!isValidRead(parts))) return false;
-        if (Objects.equals(parts[0], "fullwrite") && (!isValidFullwrite(parts))) return false;
-        if (Objects.equals(parts[0], "write") && (!isValidWrite(parts))) return false;
+        if (checkRegisteredCommand(parts[0])) return true;
+        if (Arrays.asList(ONE_LENGTH_COMMAND).contains(parts[0])) return isValidOneLength(parts);
+        if (Arrays.asList(COMMAND_LBA).contains(parts[0])) return isValidCmdLBA(parts);
+        if (Arrays.asList(COMMAND_DATA).contains(parts[0])) return isValidCmdData(parts);
+        if (Arrays.asList(COMMAND_LBA_DATA).contains(parts[0])) return isValidCmdLBAData(parts);
 
-        return true;
+        return false;
     }
 
     private boolean checkRegisteredCommand(String command) {
-        String[] candidate = {"read", "write", "fullread", "fullwrite", "help", "exit"};
-        return Arrays.asList(candidate).contains(command);
+        if (!Arrays.asList(REGISTERED_COMMAND).contains(command)) return true;
+        if (!command.matches("[a-z]+")) return true;
+
+        return false;
     }
 
-    private boolean isValidRead(String[] parts) {
-        // read 3
-        if (parts.length != 2) return false;
-        if (isValidLBAPositionArgument(parts[1])) return false;
+    private boolean isValidOneLength(String[] parts) {
+        if (parts.length != 1) return true;
 
-        return true;
+        return false;
     }
 
-    private boolean isValidFullwrite(String[] parts) {
-        // fullwrite 0xSSSSSSSS
-        if (parts.length != 2) return false;
-        if (isValidDataArgument(parts[1])) return false;
+    private boolean isValidCmdLBA(String[] parts) {
+        // COMMAND LBA (ex. read 3)
+        if (parts.length != 2) return true;
+        if (isValidLBAPositionArgument(parts[1])) return true;
 
-        return true;
+        return false;
     }
 
-    private boolean isValidWrite(String[] parts) {
-        // write 3 0xSSSSSSSS
-        if (parts.length != 3) return false;
-        if (isValidLBAPositionArgument(parts[1])) return false;
-        if (isValidDataArgument(parts[2])) return false;
+    private boolean isValidCmdData(String[] parts) {
+        // COMMAND DATA (ex. fullwrite 0xSSSSSSSS)
+        if (parts.length != 2) return true;
+        if (isValidDataArgument(parts[1])) return true;
 
-        return true;
+        return false;
+    }
+
+    private boolean isValidCmdLBAData(String[] parts) {
+        // COMMAND LBA DATA (ex. write 3 0xSSSSSSSS)
+        if (parts.length != 3) return true;
+        if (isValidLBAPositionArgument(parts[1])) return true;
+        if (isValidDataArgument(parts[2])) return true;
+
+        return false;
     }
 
     private boolean isValidLBAPositionArgument(String arg) {
@@ -69,7 +83,7 @@ public class TestShell {
     private boolean isValidDataArgument(String arg) {
         if (!arg.startsWith("0x")) return true;
         if (arg.length() != 10) return true;
-        if (!arg.substring(2).matches("[A-Z0-9]+")) return true;
+        if (!arg.substring(2).matches("[A-F0-9]+")) return true;
 
         return false;
     }
