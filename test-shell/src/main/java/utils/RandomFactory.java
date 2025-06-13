148479f3031d27d388fc2f00438dd8da6603ea06
sunw.moon <sunw.moon@samsung.com>
2025-06-13 12:30:53 +0900

    [refactor]: 주석 추가

--- test-shell/src/main/java/utils/RandomFactory.java
+++ test-shell/src/main/java/utils/RandomFactory.java
@@ -3,6 +3,7 @@ package utils;
 import java.util.Random;
 
 public class RandomFactory {
+    // Mocking을 위해 static으로 구현하지 않음
     public String getRandomHexValue() {
         Random random = new Random();
         int randomInt = random.nextInt();
