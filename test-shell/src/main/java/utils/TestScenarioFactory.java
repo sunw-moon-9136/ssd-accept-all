ef2f0caec4c8d8be9693d47e375c4f25da7de9fa
sunw.moon <sunw.moon@samsung.com>
2025-06-13 10:53:37 +0900

    [refactor]: 복잡한 if문 switch로 변경

--- test-shell/src/main/java/utils/TestScenarioFactory.java
+++ test-shell/src/main/java/utils/TestScenarioFactory.java
@@ -4,16 +4,18 @@ import scenario.*;
 import shell.manager.IManager;
 
 public class TestScenarioFactory {
+
     public static ITestScenario getTestScenario(String command, IManager manager) {
-        if (command.equals("1_FullWriteAndReadCompare") || command.equals("1_")) {
-            return FullWriteAndReadCompare.getInstance(manager);
-        } else if (command.equals("2_PartialLBAWrite") || command.equals("2_")) {
-            return PartialLBAWrite.getInstance(manager);
-        } else if (command.equals("3_WriteReadAging") || command.equals("3_")) {
-            return WriteReadAging.getInstance(manager);
-        } else if (command.equals("4_EraseAndWriteAging") || command.equals("4_")) {
-            return EraseAndWriteAging.getInstance(manager);
-        }
-        return null;
+        return switch (command) {
+            case "1_FullWriteAndReadCompare", "1_"
+                    -> FullWriteAndReadCompare.getInstance(manager);
+            case "2_PartialLBAWrite", "2_"
+                    -> PartialLBAWrite.getInstance(manager);
+            case "3_WriteReadAging", "3_"
+                    -> WriteReadAging.getInstance(manager);
+            case "4_EraseAndWriteAging", "4_"
+                    -> EraseAndWriteAging.getInstance(manager);
+            default -> null;
+        };
     }
 }
