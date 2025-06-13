81b92e2e217ff7c4bb0087ae26f908aa831c0438
sunw.moon <sunw.moon@samsung.com>
2025-06-13 13:33:02 +0900

    [fix]: TestRunner Fail 시 바로 return 되도록 수정 및 Unit Test 보강

--- test-shell/src/test/java/scenario/TestRunnerTest.java
+++ test-shell/src/test/java/scenario/TestRunnerTest.java
@@ -1,58 +1,115 @@
 package scenario;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import shell.Processor;
 import shell.manager.IManager;
-import shell.manager.Manager;
-import shell.output.Output;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TestRunnerTest {
+    public static final String TEST_SCRIPT_SAMPLE = "1_FullWriteAndReadCompare\n" +
+            "2_PartialLBAWrite\n" +
+            "3_WriteReadAging\n" +
+            "4_EraseAndWriteAging";
 
-    TestRunner T;
+    @Spy
+    TestRunner testRunner;
+
     @Mock
     IManager mockManger;
 
-    @Mock
-    Output mockOutput;
-
-    @Mock
-    Processor mockProcessor;
-
-    @BeforeEach
-    void setUp() {
-        mockProcessor = new Processor();
-        mockOutput = new Output();
-        mockManger = new Manager(mockProcessor, mockOutput);
+    @AfterEach
+    void cleanUp() throws IOException {
+        Files.deleteIfExists(Path.of(TestRunner.TEST_SCRIPT_TXT_FILE_NAME));
     }
 
-    @Test
-    void testscript_파일가져오는지_확인() {
-        T = new TestRunner("C:\\Users\\User\\Documents\\test.txt");
-        T.readTestScriptFile();
-        int expected = 2;
-        int act = T.testScenarios.size();
-        for (String o : T.testScenarios) {
-            System.out.println(o);
+    @Nested
+    class ReadTestScriptFileTest {
+
+        @Test
+        void 정상적으로_read한_경우() throws IOException {
+            int expected = 4;
+            Files.writeString(Path.of(TestRunner.TEST_SCRIPT_TXT_FILE_NAME),
+                    TEST_SCRIPT_SAMPLE);
+
+            testRunner.readTestScriptFile();
+
+            assertThat(testRunner.getTestScenarios().size()).isEqualTo(expected);
+            assertThat(String.join("\n", testRunner.getTestScenarios()))
+                    .contains(TEST_SCRIPT_SAMPLE);
         }
-        assertEquals(act, expected);
 
+        @Test
+        void 파일이_없는_경우() {
+            testRunner.readTestScriptFile();
+
+            assertThat(testRunner.getTestScenarios()).isEmpty();
+        }
     }
 
-    @Test
-    void TestScript실행하는지확인() {
-        T = new TestRunner("C:\\Users\\User\\Documents\\test.txt");
-        T.readTestScriptFile();
-        T.process(mockManger);
-        //verify(T,times(2)).()
+    @Nested
+    class ProcessTest {
 
+        final int EXPECTED_SCRIPTS_SIZE = 4;
+
+        @BeforeEach
+        void setUp() throws IOException {
+            Files.writeString(Path.of(TestRunner.TEST_SCRIPT_TXT_FILE_NAME),
+                    TEST_SCRIPT_SAMPLE);
+        }
+
+        @Test
+        void TestScript가_N개_있는_경우() {
+            doReturn(true).when(testRunner).run(any());
+
+            testRunner.process(mockManger);
+
+            verify(testRunner, times(EXPECTED_SCRIPTS_SIZE)).run(any());
+        }
+
+        @Test
+        void TestScript가_없는_경우() throws IOException {
+            Files.deleteIfExists(Path.of(TestRunner.TEST_SCRIPT_TXT_FILE_NAME));
+
+            testRunner.process(mockManger);
+
+            verify(testRunner, times(0)).run(any());
+        }
+
+        @Test
+        void 잘못된_TestScript가_존재하는_경우() {
+            doReturn(false).when(testRunner).run(any());
+
+            testRunner.process(mockManger);
+
+            verify(testRunner, times(1)).run(any());
+        }
+
+        @Test
+        void TestScript가_Fail된_경우() throws IOException {
+            final String WRONG_TEST_SCRIPT = "1_FullWriteAndReadCompare\n\n" +
+                    "WRONG_TEXT\n" +
+                    "3_WriteReadAging\n" +
+                    "4_EraseAndWriteAging";
+            Files.writeString(Path.of(TestRunner.TEST_SCRIPT_TXT_FILE_NAME),
+                    WRONG_TEST_SCRIPT, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+            doReturn(false).when(testRunner).run(any());
+
+            testRunner.process(mockManger);
+
+            verify(testRunner, times(1)).run(any());
+        }
     }
-
-
 }
